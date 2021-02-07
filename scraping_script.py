0ded64b4a2d3e6254b45da5de95ca9f7b183a3f3
JulianUmbhau <jensen_303@hotmail.com>
2021-02-07 12:14:13 +0100

    linkscrape working

--- scraping_script.py
+++ scraping_script.py
@@ -11,114 +11,143 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-
-from bs4 import BeautifulSoup
+import time
 import pandas as pd
 
 #browser = webdriver.Firefox()
 
 chrome_options = Options()
-#chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--remote-debugging-port=9222")
 
-driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
+driver_path = '/usr/bin/chromedriver'
 
-#driver = webdriver.Chrome()
+dc_codes = {"rolling_stock":2234,
+            "rail_network":3430,
+            "rail_transport":4514,
+            "vehicle_on_rails":4657}
 
-dc_codes = {"rail_network":3430,
-            "rail_transport":4514}
 
-code = "DC_CODED=" + str(dc_codes["rail_network"])
 
-driver.get("https://eur-lex.europa.eu/search.html?"+ code +"&SUBDOM_INIT=ALL_ALL&DTS_SUBDOM=ALL_ALL&DTS_DOM=ALL&lang=en&type=advanced&qid=1612306865594")
-
-driver.find_element_by_class_name("SearchResult").find_element_by_class_name("title")
-driver.find_element_by_xpath("//div[@class='SearchResult']").find_element_by_xpath(".//a[@class='title']").text
-driver.find_element_by_class_name("wt-cck-btn-add").click()
-
-results = pd.DataFrame()
-
-doc_date_end_list = []
-doc_date_start_list = []
-link_list = []
-titles_list = []
-i=0
-delay = 3
-# går igennem dokument en for en
-
-while True:
-    next_page_btn = driver.find_elements_by_xpath("//a[@title='Next Page']")
-    if len(next_page_btn) <1:
-        print("no more pages left")
-        break
-    else:
-        urls = driver.find_elements_by_xpath("//*[@class='iUh30']")
-        urls = [url.text for url in urls]
+def scrape_links(search_term, driver_path, chrome_options):
+    driver = webdriver.Chrome(driver_path, options=chrome_options)
     
-    
-### Gå igennem searchresults loop og få title og name
-    
-        urls = driver.find_element_by_xpath("//*[@class='SearchResult']").find_elements_by_xpath(".//*[class='title']")
-    
-        titles_list.append(element.find_element_by_xpath(".//a[@class='title']").text)
-        link_list.append(element.find_element_by_xpath(".//a[@class='title']").get_attribute('name'))
+    ### HACK HACK - PUT IN DATA FILE
+    dc_codes = {"rolling_stock": 2234,
+                "rail_network": 3430,
+                "rail_transport": 4514,
+                "vehicle_on_rails": 4657}
 
-        print(urls)
-
-
-
-
-    element = WebDriverWait(driver,5).until(expected_conditions.element_to_be_clickable((By.ID,'Next Page')))
-    driver.execute_script("return arguments[0].scrollIntoView();", element)
-    element.click()
-
-
-
-
-for element in driver.find_elements_by_xpath("//div[@class='SearchResult']"): 
-    i += 1
-    print(i)
-    titles_list.append(element.find_element_by_xpath(".//a[@class='title']").text)
-    link_list.append(element.find_element_by_xpath(".//a[@class='title']").get_attribute('name'))
-    
-    link_list
-
-    driver.find_element_by_class_name("title").click()    
-    driver.find_element_by_link_text("Document information").click()
-    document_date = driver.find_elements_by_class_name("NMetadata")
-    document_date[0].text
-    element_text = driver.find_element_by_id("PPDates_Contents").text
-    element_text = element_text.split("\n")
-    doc_date_start_list.append(element_text[0:2])
-    doc_date_end_list.append(element_text[6:8])
+    ####
+    delay = 5
+    dc_code = str(dc_codes[search_term])
+    driver.get("https://eur-lex.europa.eu/search.html?DC_CODED="+ dc_code +"&SUBDOM_INIT=ALL_ALL&DTS_SUBDOM=ALL_ALL&DTS_DOM=ALL&lang=en&type=advanced&qid=1612306865594")
     try:
-        go_back_elem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "PPDates_Contents")))
-        go_back_elem.click()
-        driver.back()
-        driver.back()
-    except TimeoutException:
-        print("Error - too long")
+        element = WebDriverWait(driver,delay).until(EC.element_to_be_clickable((By.CLASS_NAME,"wt-cck-btn-add"))).click()
+    except:
+        print("ok")
     
+    i=0
     
-    
-    # går igennem metadata og extracter
-for desc_list in driver.find_elements_by_class_name("NMetadata"):
-        print(desc_list.text)
-        test = desc_list
-        #print(desc_list.find_element_by_xpath("./dt['Date of document']/following-sibling::dd").text)
-    
-    break
+    # går igennem dokument en for en
+    df = pd.DataFrame(columns=("search_term", "title","link"))
+                
+    while True:
+        
+        try:
+            driver.find_element_by_xpath("//*[@class='alert alert-info']")
+            print("refreshing")
+            driver.refresh()
+            time.sleep(delay)
+        except:
+            pass
+        urls = driver.find_elements_by_xpath("//*[@class='SearchResult']")
+        for url in urls:
+            temp = [str(search_term), 
+                    url.find_element_by_xpath(".//*[@class='title']").text, 
+                    url.find_element_by_xpath(".//*[@class='title']").get_attribute("name")]
+            df.loc[len(df)] = temp
+            i += 1
+            print(i)
+        next_page_btn = driver.find_elements_by_xpath("//a[@title='Next Page']")
+        if len(next_page_btn) <1:
+            print("no more pages left")
+            break
+        element = WebDriverWait(driver,delay).until(EC.element_to_be_clickable((By.XPATH,"//*[@title='Next Page']")))
+        driver.execute_script("return arguments[0].scrollIntoView();", element)
+        element.click()
+    driver.close()
+    return(df)
+
+search_term = "rail_transport"
+df = scrape_links(search_term, driver_path, chrome_options)
 
 
-# kan beautifulsoup bruges?
-import requests
-response = requests.get("https://eur-lex.europa.eu/legal-content/EN/ALL/?uri=CELEX:32020R2222&qid=1612306865594")
-page = response.text
-soup = BeautifulSoup(page, "html.parser")
-print(soup.prettify())
+
+df = pd.DataFrame()
+for key in dc_codes.keys():
+    print(key)
+    search_term = key
+    df_temp = scrape_links(search_term = search_term, driver_path=driver_path, chrome_options=chrome_options)
+    df = df.append(df_temp)
+
+
+link_list_path = "./link_list.csv"
+df.to_csv(link_list_path)
+
+
+
+
+
+
+
+
+
+
+
+# df = pd.read_csv(link_list_path, index_col=0)
+
+# ### getting information on documents based on links
+# chrome_options = Options()
+# #chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--disable-dev-shm-usage')
+# chrome_options.add_argument("--remote-debugging-port=9222")
+
+# driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
+
+# link_list_clean = []
+# for link in link_list:
+#     link_list_clean.append(link.split(":")[2])
+
+
+# doc_date_list = []
+
+
+# for link in link_list_clean:
+#     driver.get(str("https://eur-lex.europa.eu/legal-content/EN/ALL/?uri=CELEX:" + link))
+#     try:
+#         element = WebDriverWait(driver,delay).until(EC.element_to_be_clickable((By.CLASS_NAME,"wt-cck-btn-add"))).click()
+#         print("accepted")
+#     except:
+#         print("accept not found")
+    
+#     print(driver.find_element_by_xpath("//*[@class='panel-group']").find_element_by_xpath(".//*[@id='title']").text)
+#     print("")
+#     print(driver.find_element_by_id("PPDates_Contents").text)
+#     doc_date_list.append(driver.find_element_by_id("PPDates_Contents").text)
+#     print("")
+
+
+
+
+
+
+# What other information is needed?    
+
+
 
 # TODO hente metadata robust
 # TODO gemme data i fil - csv
